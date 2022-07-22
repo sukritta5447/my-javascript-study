361f91c63daf69d415315d191fcaa26c6bf13f88
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2022-07-23 03:16:30 +0700

    Update day2.js

--- day2.js
+++ day2.js
@@ -1,1 +1,9 @@
+// Exercise day 2
 
+let challenge = '30 days of JavaSript';
+console.log(challenge); // result: 30 days of JavaSript
+console.log(challenge.length); // result: 21
+console.log(challenge.toUpperCase()); // 30 DAYS OF JAVASCRIPT
+console.log(challenge.toLowerCase()); // 30 days of javascript
+console.log(challenge.substr(0,2)); // 30
+console.log(challenge.substring(3,21)); // days of JavaScript
