69334de66ffa70734b3d67c4f1046ab77a5f26bc
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2022-07-23 03:19:18 +0700

    Update day2.js

--- day2.js
+++ day2.js
@@ -7,3 +7,4 @@ console.log(challenge.toUpperCase()); // 30 DAYS OF JAVASCRIPT
 console.log(challenge.toLowerCase()); // 30 days of javascript
 console.log(challenge.substr(0,2)); // 30
 console.log(challenge.substring(3,21)); // days of JavaScript
+console.log(challenge.includes('Script')); // true 
