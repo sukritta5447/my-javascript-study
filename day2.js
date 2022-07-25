ab817b02abd016ace255c57424dce2f9775cee6e
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2022-07-26 03:17:10 +0700

    Update day2.js

--- day2.js
+++ day2.js
@@ -8,7 +8,19 @@ console.log(challenge.toLowerCase()); // 30 days of javascript
 console.log(challenge.substr(0,2)); // 30
 console.log(challenge.substring(3,21)); // days of JavaScript
 console.log(challenge.includes('Script')); // true 
-console.log(challenge.split()); // ['30 days of JavaSript']
-console.log(challenge.split(' ')); // ['30', 'days', 'of', 'JavaSript']
+console.log(challenge.split()); // ['30 days of JavaScript']
+console.log(challenge.split(' ')); // ['30', 'days', 'of', 'JavaScript']
 
-// continue with Ex.11
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+
+console.log(companies.split(',')); // ['Facebook', ' Google', ' Microsoft', ' Apple', ' IBM', ' Oracle', ' Amazon']
+
+console.log(challenge.replace('JavaScript', 'Python')); // 30 days of Python
+console.log(challenge.charAt(15)); // S
+console.log(challenge.charCodeAt('J')); // 51
+console.log(challenge.indexOf('a')); // 4
+console.log(challenge.lastIndexOf('a')); // 14
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+
+console.log(sentence.indexOf('because')); // 31
