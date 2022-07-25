7b6a081bc3e8ed4a9b707180e6e65123a689ec0e
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2022-07-26 03:39:42 +0700

    Update day2.js

--- day2.js
+++ day2.js
@@ -24,3 +24,23 @@ console.log(challenge.lastIndexOf('a')); // 14
 let sentence = 'You cannot end a sentence with because because because is a conjunction';
 
 console.log(sentence.indexOf('because')); // 31
+console.log(sentence.lastIndexOf('because')); // 47
+console.log(sentence.search('because')); // 31
+
+let challenges = ' 30 days of JavaScript ';
+console.log(challenge.trim()); // '30 days of JavaScript'
+
+console.log(challenge.startsWith(3)); // true
+console.log(challenge.endsWith('t')); // true
+
+let challenge = '30 days of JavaScript';
+let findA = /a/gi; // g-means to search in the whole text, i - case insensitive
+console.log(challenge.match(findA)); // (3) ['a', 'a', 'a']
+
+let txtOne = '30 Days of';
+console.log(txtOne.concat(" JavaScript")); // 30 Days of JavaScript
+
+console.log(challenge.repeat(2)); // 30 days of JavaScript30 days of JavaScript
+
+
+
