aad6543ddc75a6fead9fdf7f5e1fd25017d539bd
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2022-07-27 05:05:53 +0700

    Update day2 level 2.js

--- day2 level 2.js
+++ day2 level 2.js
@@ -1,1 +1,21 @@
+// Exercise Level 2
+
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.'); // The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\""); // "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+
+console.log(typeof '10'); // string --not a number so, we have to change it into number
+let num = '10';
+let numInt = parseInt(num);
+console.log(numInt); // 10 --and now we got a number
+
+console.log(parseFloat('9.8')); // 9.8 --not equal to 10
+console.log(parseInt('9.8')); // 9
+
+let py = 'python';
+let ja = 'jargon';
+console.log(py.includes('on')); // true
+console.log(ja.includes('on')); // true
+
+let sentence = 'I hope this course is not full of jargon.';
+console.log(sentence.includes('jargon')); // true
 
