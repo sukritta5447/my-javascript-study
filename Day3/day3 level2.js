86a39ffd1c1c9ae2362604af722e4e99b6e190f4
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2022-12-03 02:50:44 +0700

    Update day3 level2.js

--- Day3/day3 level2.js
+++ Day3/day3 level2.js
@@ -8,3 +8,13 @@ const areaOfTriangle = 0.5 * base * height
 console.log(areaOfTriangle) // 100
 -----
   
+let sideA = 5
+let sideB = 4
+let sideC = 3
+
+const perimeterOfTriangle = sideA + sideB + sideC 
+
+console.log(perimeterOfTriangle) // 12
+-----
+  
+
