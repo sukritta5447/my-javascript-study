6a5abef80b85315a1698ca79b54e02c7eff9a55c
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2022-12-02 13:16:37 +0700

    Update day3 level1.js

--- Day3/day3 level1.js
+++ Day3/day3 level1.js
@@ -1,1 +1,77 @@
 // exercise day 3
+
+let firstName = 'Sukritta';
+let lastName = 'Butsabakornkul';
+let country = 'Thailand';
+let city = 'Chiang Mai';
+let age = 23;
+let isMarried = false;
+let year = 2022;
+
+typeof firstName; // 'string'
+typeof lastName // 'string'
+typeof country // 'string'
+typeof city // 'string'
+typeof age // 'number'
+typeof isMarried // 'boolean'
+typeof year // 'number'
+
+-----
+  
+typeof '10' // is a string
+typeof 10 // is a number
+// ans.: '10' and 10 are not equal
+
+-----
+  
+parseInt('9.8') // result: 9
+// ans.: parseInt('9.8') is not equal to 10
+-----
+
+let x = 3;
+let y = 4;
+console.log(x<y); // true
+console.log(4<5) // true
+console.log('mango' === 'mango') // true
+
+console.log(x>y) //false
+console.log(15<10) // false
+console.log('jello' === 'butter') // false
+
+-----
+
+4 > 3 // true
+4 >= 3 // true
+4 < 3 // false
+4 <= 3 // false
+4 == 4 // true
+4 === 4 // true
+4 != 4 // false
+4 !== 4 // false
+4 != '4' // true --> false
+4 == '4' // false --> true
+4 === '4' // false
+Find the length of python and jargon and make a falsy comparison statement. 
+console.log('python'.length != 'jargon'.length);
+
+-----
+  
+4 > 3 && 10 < 12 // true
+4 > 3 && 10 > 12 // false
+4 > 3 || 10 < 12 // true
+4 > 3 || 10 > 12 // true
+!(4 > 3) // false
+!(4 < 3) // true
+!(false) // true
+!(4 > 3 && 10 < 12) // false
+!(4 > 3 && 10 > 12) // true
+!(4 === '4') // true
+There is no 'on' in both dragon and python
+
+
+
+
+
+
+
+
